eae410d2b79123fbc4ffe4d72b24ccbf52af48e0
DankersW <wouter.dankers@skynet.be>
2020-08-18 14:12:53 +0200

    output formating added

--- home_server/tests/logging_tests/unittest_logging.py
+++ home_server/tests/logging_tests/unittest_logging.py
@@ -23,8 +23,17 @@ class TestLogging(unittest.TestCase):
         log_levels = [0, 20, 40, 0, 40, 50]
         min_log_lvls = [10, 10, 50, 10, 0, 40]
         truth_list = [None, 't | info : a', None, None, 't | error : a', 't | critical : a']
-        for index, log_lvl in enumerate(log_levels):
-            log.set_log_lvl(min_log_lvls[index])
+        for truth, log_lvl, min_log_lvl in zip(truth_list, log_levels, min_log_lvls):
+            log.set_log_lvl(min_log_lvl)
             helper = str(log.log(msg, log_lvl))
             test_result = helper.split(' - ')[-1]
-            self.assertEqual(test_result, str(truth_list[index]))
+            self.assertEqual(test_result, str(truth))
+
+    def test_output_format(self):
+        log = Logging(owner='t', log_mode='test')
+        truth_list = ['\033[1;35m', '\033[1;31m', '\033[0;33m', '\033[0;0m', '\033[;1m', '\033[0;0m']
+        log_levels = [log.LogLevels.critical, log.LogLevels.error, log.LogLevels.warning, log.LogLevels.info,
+                      log.LogLevels.debug, log.LogLevels.not_set]
+        for truth, log_lvl in zip(truth_list, log_levels):
+            test_result = log.get_output_format(log_lvl)
+            self.assertEqual(test_result, truth)
