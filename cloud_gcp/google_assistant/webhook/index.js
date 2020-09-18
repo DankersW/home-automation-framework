c3cbbc5cd8b2b7f8680155b7828084658e4a68a0
DankersW <wouter.dankers@skynet.be>
2020-09-18 22:40:28 +0200

    Connection to firestore works in gcp shell but same crash still on cloud functions

--- cloud_gcp/google_assistant/webhook/index.js
+++ cloud_gcp/google_assistant/webhook/index.js
@@ -1,47 +1,53 @@
-    'use strict';
+'use strict';
 
-    const functions = require('firebase-functions');
-    const {WebhookClient} = require('dialogflow-fulfillment');
-    const {Card, Suggestion} = require('dialogflow-fulfillment');
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
 
-    var serviceAccount = require("./dankers-firebase-adminsdk-r85r7-df645bc8df.json");
-    var admin = require("firebase-admin");
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: "https://dankers.firebaseio.com"
-    });
-    var db = admin.firestore();
+const serviceAccount = require("./dankers-firebase-adminsdk-r85r7-df645bc8df.json");
+//admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+//  databaseURL: "https://dankers.firebaseio.com"
+//});
+//const db = admin.firestore();
 
-    process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+//const writeDeviceData = async function(device, state){
+//  const docRef = db.collection('devices').doc(device);
+//  await docRef.set({
+//    online: true,
+//    state: state
+//  });
+//}
 
-    exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-      const agent = new WebhookClient({ request, response });
-      console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-      console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-      function welcome(agent) {
-        agent.add(`Welcome to my agent!`);
-      }
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-      function fallback(agent) {
-        agent.add(`I didnt understand`);
-        agent.add(`Im sorry, can you try again?`);
-      }
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
 
-      function setLights(agent) {
-        agent.add(`Responds from webhook`);
+  function fallback(agent) {
+    agent.add(`I didnt understand`);
+    agent.add(`Im sorry, can you try again?`);
+  }
 
-        process.stdout.write("hello: ");
+  function setLights(agent) {
+    agent.add(`Responds from webhook`);
 
-        let devices = db.collection("devices");
-        process.stdout.write("devices: %j", devices);
-      }
+    console.log("hello: ");
 
-      // Run the proper function handler based on the matched Dialogflow intent name
-      let intentMap = new Map();
-      intentMap.set('Default Welcome Intent', welcome);
-      intentMap.set('Default Fallback Intent', fallback);
-      intentMap.set('home automation', setLights);
+    //writeDeviceData('light_switch_001', 1)
+  }
 
-      agent.handleRequest(intentMap);
-    });
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('home automation', setLights);
+
+  agent.handleRequest(intentMap);
+});
