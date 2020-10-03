e0b4eb244ff7a7c3f8861060353991621c6ec9eb
DankersW <wouter.dankers@skynet.be>
2020-10-03 11:26:12 +0200

    f

--- src/lib/configuration_parser.py
+++ src/lib/configuration_parser.py
@@ -19,11 +19,12 @@ class ConfigurationParser:
             dir_structure[0] += f'\\'
         elif dir_structure[0] == '':
             dir_structure[0] = '/'
-        #index_repo_name = [i for i, x in enumerate(dir_structure) if x == self.repo_name][-1]
-        index_repo_name = dir_structure.index(self.repo_name)
+        index_repo_name = [i for i, x in enumerate(dir_structure) if x == self.repo_name][-1]
+        #index_repo_name = dir_structure.index(self.repo_name)
         print(index_repo_name)
         project_home = Path(*dir_structure[:index_repo_name + 1])
-        print(f'home location {system(f"ls -lS /{project_home}")}')
+        print(f'project home {project_home}')
+        print(f'home location {system(f"ls -lS /{project_home}/")}')
         pat = Path.joinpath(project_home, 'configuration.yml')
         print(f'Path to file : {pat}')
         return pat
