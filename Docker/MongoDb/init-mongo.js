eb4ed6f5729668f04e89e9bd0b1a231eb9912b97
DankersW <wouter.dankers@skynet.be>
2020-12-27 22:04:48 +0100

    express mongo added

--- Docker/MongoDb/init-mongo.js
+++ Docker/MongoDb/init-mongo.js
@@ -1,5 +1,6 @@
-db.auth('admin', 'mongo_admin_iot')
+//db.auth('admin', 'mongo_admin_iot')
 
+/*
 db.createUser(
     {
         user: "admin",
@@ -11,3 +12,14 @@ db.createUser(
         ]
     }
 )
+*/
+
+
+db = db.getSiblingDB('iot_db');
+db.createUser(
+    {
+        user: 'admin',
+        pwd: 'mongo_admin_iot',
+        roles: [{ role: 'readWrite', db: 'api_prod_db' }],
+    },
+);
