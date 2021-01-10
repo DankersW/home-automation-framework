f104c64d30f1bbe19f6439c5f4259c44bff878d2
DankersW <wouter.dankers@skynet.be>
2021-01-10 22:25:59 +0100

    Handleling mqtt on message

--- src/iot_gateway/mqtt_gateway.py
+++ src/iot_gateway/mqtt_gateway.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
 from queue import Queue
 from threading import Thread, Event
+from json import loads
+from datetime import datetime
 
 from src.iot_gateway.mqtt_client import MqttClient
 
 from src.logging.logging import Logging
 from lib.configuration_parser import ConfigurationParser
+from lib.utils import is_json
 
 
 @dataclass
@@ -53,8 +56,51 @@ class MqttGateway(Thread):
         self._thread_ready.set()
         self.running = True
 
-    def on_message(self, topic, payload):
+    def on_message(self, topic: str, payload: str) -> None:
+        self.log.info(f'Received {payload!r} on topic {topic!r}')
+        self._log_mqtt_traffic(topic=topic, payload=payload)
+
+        data = self._parse_mqtt_payload(payload=payload)
+        if self._is_valid_mqtt_message(msg=data):
+            self._message_handler(data=data)
+        else:
+            self.log.warning('The MQTT message is was not valid')
+
+    def _log_mqtt_traffic(self, topic: str, payload: str) -> None:
+        msg = {'timestamp': datetime.now(), 'source': type(self).__name__, 'topic': topic, 'payload': payload}
+        traffic_item = {'event': 'iot_traffic', 'message': msg}
+        self._observer_publish_queue.put(traffic_item)
+
+    def _parse_mqtt_payload(self, payload: str) -> dict:
+        if not is_json(payload):
+            self.log.warning('Received message was not in JSON format.')
+            return dict()
+        return loads(payload)
+
+    @staticmethod
+    def _is_valid_mqtt_message(msg: dict) -> bool:
+        needed_keys = ['device_id', 'event_type']
+        for key in needed_keys:
+            if key not in msg:
+                return False
+        return True
+
+    def _message_handler(self, data: dict) -> None:
+        action_map = {
+            'iot_dev_state_change': self._handle_state_change
+        }
+        event = data.get('event_type')
+        action = action_map.get(event, self._unknown_event)
+        action(data)
+
+    def _unknown_event(self, data: dict):
+        self.log.warning(f'Unknown event {data.get("event_type")} - No action selected')
+
+    def _handle_state_change(self, data: dict):
         pass
+
+# todo: finish message handler
+# todo: inplement handle_state_changee
 
 
 if __name__ == '__main__':
--- tests/test_mqtt_gateway.py
+++ tests/test_mqtt_gateway.py
@@ -1,9 +1,60 @@
 from unittest import TestCase, mock
 from queue import Queue
 from threading import Event
+from json import dumps
 
 from src.iot_gateway.mqtt_gateway import MqttGateway
+from tests.helper_functions import emtpy_queue
 
 
 class TestMqttGateway(TestCase):
-    pass
+    test_queue = Queue(10)
+    default_event = Event()
+
+    @mock.patch("src.iot_gateway.mqtt_client.MqttClient")
+    def test_log_mqtt_traffic(self, mock_mqtt_client):
+        """ Testing logging of a mqtt message"""
+        mock_mqtt_client.return_value = None
+        topic = 'test'
+        payload = '1-2-three'
+        mqtt_gateway = MqttGateway(queue=self.test_queue, thread_event=self.default_event)
+
+        emtpy_queue(queue=self.test_queue)
+        mqtt_gateway._log_mqtt_traffic(topic=topic, payload=payload)
+        result = self.test_queue.get()
+        self.assertEqual(result['event'], 'iot_traffic')
+        self.assertEqual(result['message']['source'], 'MqttGateway')
+        self.assertEqual(result['message']['topic'], topic)
+        self.assertEqual(result['message']['payload'], payload)
+
+    @mock.patch("src.iot_gateway.mqtt_client.MqttClient")
+    def test_parse_mqtt_payload_correct(self, mock_mqtt_client):
+        mock_mqtt_client.return_value = None
+        mqtt_gateway = MqttGateway(queue=self.test_queue, thread_event=self.default_event)
+
+        payload = {'test': 123}
+        result = mqtt_gateway._parse_mqtt_payload(payload=dumps(payload))
+        self.assertEqual(result, payload)
+
+    @mock.patch("src.iot_gateway.mqtt_client.MqttClient")
+    def test_parse_mqtt_payload_incorrect(self, mock_mqtt_client):
+        mock_mqtt_client.return_value = None
+        mqtt_gateway = MqttGateway(queue=self.test_queue, thread_event=self.default_event)
+
+        result = mqtt_gateway._parse_mqtt_payload(payload='payload')
+        self.assertEqual(result, dict())
+
+    def test_is_valid_mqtt_message_correct(self):
+        message = {'device_id': 'device_001', 'event_type': 'iot_dev_state_change', 'stat': True}
+        result = MqttGateway._is_valid_mqtt_message(msg=message)
+        self.assertTrue(result)
+
+    def test_is_valid_mqtt_message_device_id_missing(self):
+        message = {'device_': 'device_001', 'event_type': 'iot_dev_state_change', 'stat': True}
+        result = MqttGateway._is_valid_mqtt_message(msg=message)
+        self.assertFalse(result)
+
+    def test_is_valid_mqtt_message_event_type_missing(self):
+        message = {'device_id': 'device_001', 'event_t': 'iot_dev_state_change', 'stat': True}
+        result = MqttGateway._is_valid_mqtt_message(msg=message)
+        self.assertFalse(result)
