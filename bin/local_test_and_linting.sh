c4d7d9dc61fdf3d7e0382423ee93a68280c35391
DankersW <wouter.dankers@skynet.be>
2021-01-21 16:29:39 +0100

    removing legacy bakcticks from linting script

--- bin/local_test_and_linting.sh
+++ bin/local_test_and_linting.sh
@@ -14,6 +14,6 @@ sub_dirs=("db"  "iot_gateway"  "logging" "host_health" "framework" "utils")
 for sub_dir in ${sub_dirs[*]}; do
   echo "Linting for src/$sub_dir"
   cd "$home_dir/home_automation_framework/$sub_dir" || { echo "cd error, check location"; exit 1; }
-  pylint -j 2 --rcfile="$home_dir/.github/workflows/setup.cfg" --output-format=colorized `ls -R | grep .py$ | xargs`
+  pylint -j 2 --rcfile="$home_dir/.github/workflows/setup.cfg" --output-format=colorized $(ls -R | grep .py$ | xargs)
 done
 cd "$home_dir" || { echo "cd error, check location"; exit 1; }
