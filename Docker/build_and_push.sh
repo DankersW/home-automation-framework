9fe21e2ed88d67f1f0bd71624a7e25ef5fe1e81a
DankersW <wouter.dankers@skynet.be>
2021-01-25 15:35:06 +0100

    cleaning up docker push script

--- Docker/build_and_push.sh
+++ Docker/build_and_push.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
-set -e
-REG_HOST="esw-artifactory.got.volvo.net"
-NAME="ocp-webhook"
-TAG=$(git describe --always)
+NAME="home_automation_framework"
+TAG="v0.1"
 SHORT_NAME=$NAME:$TAG
-FULL_NAME=$REG_HOST:5000/$SHORT_NAME
-cd .. # Hacky solution to solve include paths in the docker file
-docker build --build-arg TAG=$TAG -t $SHORT_NAME -f docker/Dockerfile .
+DOCKER_REPO="dankersw/"$SHORT_NAME
+
+echo "Building docker image"
+docker build --build-arg TAG=$TAG -t $SHORT_NAME ..
+
 echo "Tagging for local registry and pushing image/tags"
-docker tag $SHORT_NAME $FULL_NAME
-
-docker login -u "${artifactory_api_username}" -p "${artifactory_api_key}" $REG_HOST:5000
-
-docker push $FULL_NAME
+docker tag $SHORT_NAME $DOCKER_REPO
+docker push $DOCKER_REPO
 docker rmi $SHORT_NAME
