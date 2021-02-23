9679d8287a07e8b87306a08ac5f699916e8b8d3c
DankersW <wouter.dankers@skynet.be>
2021-02-23 15:21:14 +0100

    Switched from Unittest to Pytest

--- bin/linting.sh
+++ bin/linting.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 home_dir=$(pwd)
 
-echo "Installing packages"
-pip3 install -r requirements.txt
-
-
-echo "Running unit tests"
-coverage run -m unittest discover -s tests/ -p 'test_*.py'
-
-echo "Generating coverage report"
-#coverage report -m
-
-: '
 echo "Linting of home_automation_framework folder"
 sub_dirs=("db"  "iot_gateway"  "logging" "host_health" "framework" "utils")
 for sub_dir in ${sub_dirs[*]}; do
@@ -20,4 +9,3 @@ for sub_dir in ${sub_dirs[*]}; do
   pylint -j 2 --rcfile="$home_dir/.github/workflows/setup.cfg" --output-format=colorized $(ls -R | grep .py$ | xargs)
 done
 cd "$home_dir" || { echo "cd error, check location"; exit 1; }
-'
