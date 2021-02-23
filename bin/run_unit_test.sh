34ac76d1cfb9ea2c1771d40ec556401e3eedc5b0
DankersW <wouter.dankers@skynet.be>
2021-02-23 16:37:29 +0100

    Removing .coverage file when run locally

--- bin/run_unit_test.sh
+++ bin/run_unit_test.sh
@@ -5,3 +5,4 @@ pip3 install -r requirements.txt
 
 echo "Running unit tests"
 python3 -m pytest -x -v --color=yes tests/ --cov=home_automation_framework --cov-config bin/.coveragerc --cov-report term-missing -p no:warnings
+rm .coverage
