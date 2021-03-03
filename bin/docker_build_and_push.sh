d70e4bb8584af69f8dc58d755079ed7ffcaa7d10
DankersW <wouter.dankers@skynet.be>
2021-03-03 13:31:08 +0100

    adding authentication to build and push script

--- bin/docker_build_and_push.sh
+++ bin/docker_build_and_push.sh
@@ -3,6 +3,9 @@ DOCKER_REPO="home_automation_framework"
 DOCKER_ACC="dankersw"
 IMG_TAG="0.0.2-x86"
 
+echo "Authenticating to Docker hub"
+docker login --username=$DOCKER_ACC
+
 echo "Building docker image"
 docker build -t $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG .. --build-arg tag=$IMG_TAG
 
