b53ae24759f7336f648940a5b387fa980c528cf3
DankersW <wouter.dankers@skynet.be>
2021-04-24 10:34:06 +0200

    basic skelethon added

--- home_automation_framework/iot_gateway/device_manager.py
+++ home_automation_framework/iot_gateway/device_manager.py
@@ -21,3 +21,46 @@ Flow:
         listen for messages, on message update digital twin wih status to active
 
 """
+
+from threading import Thread, Event
+from queue import Queue
+
+from home_automation_framework.logging.logging import Logging
+
+
+class HealthMonitor(Thread):
+    running = True
+    update_time_sec = 600
+    subscribed_event = []
+
+    def __init__(self, queue: Queue, thread_event: Event) -> None:
+        Thread.__init__(self)
+        self._observer_publish_queue = queue
+        self._thread_ready = thread_event
+        self._observer_notify_queue = Queue(maxsize=100)
+        self.log = Logging(owner=__file__, config=True)
+
+    def __del__(self) -> None:
+        self.running = False
+
+    def run(self) -> None:
+        self._thread_ready.set()
+        while self.running:
+            """
+            queue_item = self._observer_notify_queue.get()
+            print(queue_item)
+            item = {'event': 'host_health', 'message': "test"}
+            self._observer_publish_queue.put(item)
+            """
+            pass
+
+    def notify(self, msg: dict, event: str) -> None:
+        self._observer_notify_queue.put(item=msg)
+
+
+if __name__ == '__main__':
+    test_queue = Queue(10)
+    t_event = Event()
+    hm = HealthMonitor(test_queue, thread_event=t_event)
+    hm.start()
+
