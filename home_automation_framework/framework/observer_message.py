6735905bd8aadd59dd28bdbbbec3f5db3f4d59ee
DankersW <wouter.dankers@skynet.be>
2021-05-06 17:51:42 +0200

    Observer message object

--- home_automation_framework/framework/observer_message.py
+++ home_automation_framework/framework/observer_message.py
@@ -1,13 +1,15 @@
 from typing import Union
+from inspect import stack
 
 
 class ObserverMessage:
-    source: str = None
-    destination: str = None
-    data: Union[str, dict] = None
-    event: str = None
-    subject: str = None
+    def __init__(self, event: str, data: Union[str, dict], subject: str = None, destination: str = None) -> None:
+        self.event = event
+        self.data = data
+        self.subject = subject
+        self.destination = destination
+        self.source = stack()[1][0].f_locals.get("self", None).__class__.__name__
 
-    def __init__(self) -> None:
-        pass
-
+    def __str__(self):
+        attrs = vars(self)
+        return f"ObserverMessage {attrs}"
