3e4cb0bb7db4b7426e2ff936640e8794388fa4b1
DankersW <wouter.dankers@skynet.be>
2021-05-09 11:08:11 +0200

    fixing up device manager unit tests

--- tests/iot_gateway/test_device_manager.py
+++ tests/iot_gateway/test_device_manager.py
@@ -1,10 +1,10 @@
 import time
-from unittest import TestCase, mock
+from unittest import TestCase
 from queue import Queue
 from threading import Event
-from json import dumps
 
 from home_automation_framework.iot_gateway.device_manager import DeviceManager
+from home_automation_framework.framework.observer_message import ObserverMessage
 from tests.helper_functions import emtpy_queue
 
 
@@ -17,18 +17,18 @@ class TestDeviceManager(TestCase):
         device_manager = DeviceManager(queue=self.test_queue, thread_event=self.default_event)
         device_manager.running = False
         device_manager.start()
-        send_msg = self.test_queue.get()
-        truth = {'event': 'digital_twin', 'message': {'action': 'fetch_digital_twin'}}
-        self.assertDictEqual(send_msg, truth)
+        msg = self.test_queue.get()
+        self.assertEqual(msg.event, "digital_twin")
+        self.assertEqual(msg.subject, "fetch_digital_twin")
 
     def test_store_remote_digital_twin(self):
         emtpy_queue(queue=self.test_queue)
         device_manager = DeviceManager(queue=self.test_queue, thread_event=self.default_event)
         device_manager.start()
         moch_dt = ["abc"]
-        notify_msg = {"action": "retrieved_digital_twin", "data": moch_dt}
+        notify_msg = ObserverMessage(event="digital_twin", subject="retrieved_digital_twin", data=moch_dt)
         device_manager.notify(msg=notify_msg, event="")
         time.sleep(0.5)
         self.assertEqual(device_manager.remote_digital_twin, moch_dt)
         device_manager.running = False
-        device_manager.notify(msg={"stop": True}, event="")
+        device_manager.notify(msg=ObserverMessage(data="", event=""), event="")
